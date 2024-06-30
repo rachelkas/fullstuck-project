d39d341fd02212251b80ee76827e46307839d7f0
rachelkas <rachelikoskas@gmail.com>
2024-06-30 23:13:42 +0300

    /

--- client/components/navbar/Nav.tsx
+++ client/components/navbar/Nav.tsx
@@ -5,7 +5,8 @@ import { CiHeart } from "react-icons/ci";
 import { BsTelephone } from "react-icons/bs";
 import { FiUser } from "react-icons/fi";
 import { IoHomeOutline } from "react-icons/io5";
-import logo from '../../assets/images/logo.png'; // Importing the image
+import BM from '../../assets/icons/BM.svg'; // Correct path to the SVG file
+
 
 const Nav: React.FC = () => {
     return (
@@ -38,7 +39,7 @@ const Nav: React.FC = () => {
                 </li>
                 <li>
                     <Link to="/home">
-                        <img src={logo} alt="logo" />
+                        <img src={BM} alt="logo" />
                     </Link>
                 </li>
             </ul>
