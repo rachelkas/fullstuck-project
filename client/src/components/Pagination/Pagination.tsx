f4fe09f5943f33618970edb631a1a7dd3cfb9c5b
rachelkas <rachelikoskas@gmail.com>
2024-07-02 17:46:59 +0300

    .

---
+++ client/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Pagination.css';  // Import the CSS file
+
+interface PaginationProps {
+    productsPerPage: number;
+    totalProducts: number;
+    paginate: (pageNumber: number) => void;
+    currentPage: number;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ productsPerPage, totalProducts, paginate, currentPage }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <nav className='pagination'>
+            <ul>
+                {pageNumbers.map(number => (
+                    <li key={number} className={number === currentPage ? 'active' : ''}>
+                        <a onClick={() => paginate(number)} href='!#'>
+                            {number}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Pagination;
